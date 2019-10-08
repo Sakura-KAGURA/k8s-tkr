986108117ecf51c3ecca27cc94ceff7ab8f339ed
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-08 21:27:07 +0900

    Update vagrant-kubernetes.sh

--- vagrant-kubernetes.sh
+++ vagrant-kubernetes.sh
@@ -1,5 +1,6 @@
 
 # https://github.com/takara9/vagrant-kubernetes
+# 環境2 構築
 git clone -b 1.14 https://github.com/takara9/vagrant-kubernetes
 
 vagrant up
@@ -7,9 +8,14 @@ vagrant status
 vagrant ssh master
 logout
 
-
+## コマンド
 vagrant up
 vagrant halt
 vagrant destroy
 
 vagrant reload
+
+## サンプルコード
+git clone https://github.com/takara9/codes_for_lessons
+
+
